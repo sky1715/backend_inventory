bbfd11f0fc7eb5898a4872aab079fa737c93121c
K Suraj <k.suraj1715@gmail.com>
2025-07-25 20:04:04 +0530

    fc

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -9,7 +9,7 @@ exports.register = async (req, res) => {
 
     const user = await User.create({ username, password });
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-    res.status(201).json({ token });
+    res.status(201).json({ token,message: 'User registered successfully' });
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
   }
@@ -21,7 +21,7 @@ exports.login = async (req, res) => {
     const user = await User.findOne({ username });
     if (user && await user.matchPassword(password)) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-      res.json({ token });
+      res.json({ token , message: 'Login successful' });
     } else {
       res.status(401).json({ message: 'Invalid credentials' });
     }
